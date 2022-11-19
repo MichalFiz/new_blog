ab560437de445ef1434a9b4bdf63a342b52066ab
Michał <117569554+MichalFiz@users.noreply.github.com>
2022-11-19 13:34:42 +0100

    dodano req

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+import os
 
 zespoly = {
     'myszy': '3',
@@ -13,7 +14,7 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route("/blog/blog_title")
+@app.route("/blog/<blog_title>")
 def show_user_profile(blog_title):
     foo = zespoly[blog_title]
     return render_template("blog.html", foo = foo)
@@ -24,3 +25,6 @@ def another_hello():
     return "<p>Hello Fortnite!</p>"
 
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, use_reloader=True)  
